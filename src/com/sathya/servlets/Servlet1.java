8e474bdeb99184b5f8ed12fb0b35fb3c1cf130e7
Admin <Admin@192.168.100.4>
2019-02-10 15:42:27 +0530

    ravi rani --- added

--- src/com/sathya/servlets/Servlet1.java
+++ src/com/sathya/servlets/Servlet1.java
@@ -17,6 +17,7 @@ public class Servlet1 extends HttpServlet {
 		out.println("Welcome to java world");	
 		out.println("Ravi-Rani");
 		out.println("---------------");
+		out.println("Ravi Rani ---------------------");
 	}
 
 
